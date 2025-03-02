035225011d32668d9505c8dc42a76017ca6aa541
Taher Maimoon <tmaimoon18@gmail.com>
2025-03-02 15:00:01 +0530

    Created TimeTable model

---
+++ Repositories/Models/TimeTable.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Repositories.Models
+{
+    public class Timetable
+{
+    public int TimetableID { get; set; }  // Maps to c_timetableID
+    public int SubjectID { get; set; }    // Maps to c_subjectID
+    public int ClassID { get; set; }      // Maps to c_classID
+    public int DayOfWeek { get; set; }   // Maps to c_day_of_week
+    public TimeSpan StartTime { get; set; }  // Maps to c_start_time
+    public TimeSpan EndTime { get; set; }    // Maps to c_end_time
+
+    // Navigation properties
+    public Subject? Subject { get; set; }  // Represents relationship with t_subject
+    public ClassModel? ClassModel { get; set; }      // Represents relationship with t_class
+}
+
+}
