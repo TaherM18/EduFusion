08191297fac3ca6aef7ea2775915d2172e7a03b7
Taher Maimoon <tmaimoon18@gmail.com>
2025-03-03 00:12:41 +0530

    Added Students action and view

--- MVC/Controllers/AdminController.cs
+++ MVC/Controllers/AdminController.cs
@@ -22,6 +22,13 @@ namespace MVC.Controllers
             return View();
         }
 
+
+        public IActionResult Students()
+        {
+            return View();
+        }
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
